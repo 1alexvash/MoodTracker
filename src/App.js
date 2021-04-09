d9f827765bf2ac239d64cac0097b8ad40e14b440
Alexander <1alexvash@gmail.com>
2021-04-09 18:11:50 +0300

    Added a visual representation of days

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import "./scsss/main.scss";
 
 const App = () => {
+  const days = [50, 50, 40, 30, 20, 80, 100];
+
   return (
     <div className="App">
       <h1 className="heading">Mood Tracker</h1>
+      <div className="days">
+        {days.map((day, index) => (
+          <div className="day" key={index} style={{ height: `${day}%` }} />
+        ))}
+      </div>
     </div>
   );
 };
