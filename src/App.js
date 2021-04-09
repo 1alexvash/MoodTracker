b2f377eeded83898195d9a62fa4ca1188c3dc539
Alexander <1alexvash@gmail.com>
2021-04-09 10:53:34 +0300

    Added roboto fonts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from "react";
 import "./scsss/main.scss";
 
 const App = () => {
-  return <div>wassup</div>;
+  return (
+    <div className="App">
+      <h1 className="heading">Mood Tracker</h1>
+    </div>
+  );
 };
 
 export default App;
