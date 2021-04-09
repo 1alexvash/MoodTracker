289f8959be088e7437b9274629866e3a5ac95600
Alexander <1alexvash@gmail.com>
2021-04-09 18:29:24 +0300

    Now submit log function adds days to the array

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./scsss/main.scss";
 
 const App = () => {
   const [days, setDays] = useState([50, 50, 40, 30, 20, 80, 100]);
+  const [happiness, setHappiness] = useState(75);
 
   const submitLog = (event) => {
     event.preventDefault();
-    console.log("Submitting the log");
+    console.log("happiness:", happiness);
+    setDays([...days, happiness]);
   };
 
   return (
@@ -20,7 +22,13 @@ const App = () => {
       </div>
       {/* TODO: add condition if already logged */}
       <form onSubmit={submitLog} className="submit-log">
-        <input type="range" min="0" max="100" />
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={happiness}
+          onChange={(e) => setHappiness(e.target.value)}
+        />
         <textarea placeholder="Add additional information" rows="5" />
         <button type="submit">Submit Log</button>
       </form>
