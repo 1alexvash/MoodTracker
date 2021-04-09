fc6accd02fa71569e5cad73071f5140d29e6b88e
Alexander <1alexvash@gmail.com>
2021-04-09 18:25:59 +0300

    Added submitLog component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./scsss/main.scss";
 
 const App = () => {
-  const days = [50, 50, 40, 30, 20, 80, 100];
+  const [days, setDays] = useState([50, 50, 40, 30, 20, 80, 100]);
+
+  const submitLog = (event) => {
+    event.preventDefault();
+    console.log("Submitting the log");
+  };
 
   return (
     <div className="App">
@@ -13,6 +18,12 @@ const App = () => {
           <div className="day" key={index} style={{ height: `${day}%` }} />
         ))}
       </div>
+      {/* TODO: add condition if already logged */}
+      <form onSubmit={submitLog} className="submit-log">
+        <input type="range" min="0" max="100" />
+        <textarea placeholder="Add additional information" rows="5" />
+        <button type="submit">Submit Log</button>
+      </form>
     </div>
   );
 };
