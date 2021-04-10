56d31d3ca293eff32684c8d3e69c2b1ad3c52be5
Alexander <1alexvash@gmail.com>
2021-04-10 18:04:06 +0300

    Added Github pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+// TODO: import moment library
+// TODO: after adding new item to the days add date mark
+// TODO: check if the date mark is unique
+// TODO: hide submit log if the user already commited
+// FIXME: fix bug with aligning of bars
+// TODO: add some info what's the purpose of this application
+// TODO: draw an image
+// TODO: make bars clickable
+// => after click show information
+// TODO: save date to localStorage
+
 import React, { useState } from "react";
 
 import "./scsss/main.scss";
 
 const App = () => {
-  const [days, setDays] = useState([50, 50, 40, 30, 20, 80, 100]);
+  const [days, setDays] = useState([
+    {
+      happiness: 51,
+      text: "50",
+      date: new Date(),
+    },
+    {
+      happiness: 50,
+      text: "50",
+      date: new Date(),
+    },
+    {
+      happiness: 49,
+      text: "50",
+      date: new Date(),
+    },
+  ]);
   const [happiness, setHappiness] = useState(75);
 
   const submitLog = (event) => {
@@ -17,7 +44,11 @@ const App = () => {
       <h1 className="heading">Mood Tracker</h1>
       <div className="days">
         {days.map((day, index) => (
-          <div className="day" key={index} style={{ height: `${day}%` }} />
+          <div
+            className="day"
+            key={index}
+            style={{ height: `${day.happiness}%` }}
+          />
         ))}
       </div>
       {/* TODO: add condition if already logged */}
