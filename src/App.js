a6ff44365d445095e9ec709b7b59619262a16475
Alexander <1alexvash@gmail.com>
2021-04-10 19:03:40 +0300

    now saved date persist in the local storage

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 // TODO: draw a logo
 // TODO: make bars clickable
 // => after click show information
-// TODO: save date to localStorage
 
 import React, { useState } from "react";
 
@@ -15,23 +14,28 @@ const App = () => {
   const today = moment().format("dddd, MMMM Do, YYYY");
 
   const [day, setDay] = useState(null);
-  const [days, setDays] = useState([
-    {
-      happiness: 55,
-      text: "55",
-      date: moment().subtract(3, "days").format("dddd, MMMM Do, YYYY"),
-    },
-    {
-      happiness: 50,
-      text: "50",
-      date: moment().subtract(2, "days").format("dddd, MMMM Do, YYYY"),
-    },
-    {
-      happiness: 45,
-      text: "I had kinda depression, but to my lack I was able to deal with it",
-      date: moment().subtract(1, "days").format("dddd, MMMM Do, YYYY"),
-    },
-  ]);
+  const [days, setDays] = useState(
+    localStorage.days
+      ? JSON.parse(localStorage.days)
+      : [
+          {
+            happiness: 55,
+            text: "55",
+            date: moment().subtract(3, "days").format("dddd, MMMM Do, YYYY"),
+          },
+          {
+            happiness: 50,
+            text: "50",
+            date: moment().subtract(2, "days").format("dddd, MMMM Do, YYYY"),
+          },
+          {
+            happiness: 45,
+            text:
+              "I had kinda depression, but to my lack I was able to deal with it",
+            date: moment().subtract(1, "days").format("dddd, MMMM Do, YYYY"),
+          },
+        ]
+  );
   const [happiness, setHappiness] = useState(75);
   const [text, setText] = useState("");
 
@@ -39,14 +43,19 @@ const App = () => {
 
   const submitLog = (event) => {
     event.preventDefault();
-    setDays([
+
+    const updatedDays = [
       ...days,
       {
         happiness,
         text,
         date: moment().format("dddd, MMMM Do, YYYY"),
       },
-    ]);
+    ];
+
+    setDays(updatedDays);
+    localStorage.days = JSON.stringify(updatedDays);
+
     setText("");
   };
 
